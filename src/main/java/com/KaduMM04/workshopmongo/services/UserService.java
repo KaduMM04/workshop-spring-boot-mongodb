572c17fb1ba3482a9e746d372abf705629660227
Carlos Eduardo <kadumendesmiranda@gmail.com>
2024-03-01 19:14:27 -0300

    User insertion with post

--- src/main/java/com/KaduMM04/workshopmongo/services/UserService.java
+++ src/main/java/com/KaduMM04/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.KaduMM04.workshopmongo.domain.User;
+import com.KaduMM04.workshopmongo.dto.UserDTO;
 import com.KaduMM04.workshopmongo.repository.UserRepository;
 import com.KaduMM04.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não Encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objdto) {
+		return new User(objdto.getId(), objdto.getName(), objdto.getEmail());
 	}
 }
