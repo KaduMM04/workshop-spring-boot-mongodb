c08647d6df70651fb7c30c5ca60745c85934b945
Carlos Eduardo <kadumendesmiranda@gmail.com>
2024-03-04 22:43:04 -0300

    Adding comments to posts.

--- src/main/java/com/KaduMM04/workshopmongo/config/Instantiation.java
+++ src/main/java/com/KaduMM04/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.KaduMM04.workshopmongo.domain.Post;
 import com.KaduMM04.workshopmongo.domain.User;
 import com.KaduMM04.workshopmongo.dto.AuthorDTO;
+import com.KaduMM04.workshopmongo.dto.CommentDTO;
 import com.KaduMM04.workshopmongo.repository.PostRepository;
 import com.KaduMM04.workshopmongo.repository.UserRepository;
 
@@ -26,7 +27,7 @@ public class Instantiation implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userRepository.deleteAll();
@@ -38,10 +39,16 @@ public class Instantiation implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("2022-03-21"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new  AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("2022-03-23"), "Bom dia", "Acordei feliz hoje!", new  AuthorDTO(maria));
+		Post post1 = new Post(null, sdf.parse("21/03/2022"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new  AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2022"), "Bom dia", "Acordei feliz hoje!", new  AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2022"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2022"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2022"),  new AuthorDTO(alex));
 		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+				
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
